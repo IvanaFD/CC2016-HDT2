c4e1676b6bb5c277bf70d9346f90044ba595ae98
IvanaFD <ivanafigueroa0936@gmail.com>
2025-01-30 22:21:10 -0600

    javadoc clase pila

--- Pila.java
+++ Pila.java
@@ -10,11 +10,40 @@
  * 
  * Clase: Pila.java
  * Comportamiento esperado por una pila. 
+ * 
+ * @param <T> el tipo de elementos almacenados en la pila
  */
 public interface Pila<T> {
+
+     /**
+     * Inserta el elemento en la parte superior de la pila.
+     * @param item el elemento a agregar a la pila
+     */
     void push(T item);
+
+    /**
+     * Elimina y devuelve el elemento superior de la pila.
+     * @return el elemento eliminado
+     * @throws EmptyStackException si la pila está vacía
+     */
     T pop();
+
+    /**
+     * Devuelve el elemento de la parte superior de la pila sin eliminarlo.
+     * @return el elemento superior de la pila
+     * @throws EmptyStackException si la pila está vacía
+     */
     T peek();
+
+    /**
+     * Verifica si esta vacia la pila
+     * @return true si la pila está vacía,false la pila está llena
+     */
     boolean isEmpty();
+
+    /**
+     * da el numero de elementos en la pila
+     * @return tamanio de la pila
+     */
     int size();
 }
