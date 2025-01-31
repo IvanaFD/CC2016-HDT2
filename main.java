090a7263d55b12b1dbe86ef9dc25cb1342153384
Diana-Sosa18 <sosadiana364@gmail.com>
2025-01-30 18:20:48 -0600

    evaluar

--- main.java
+++ main.java
@@ -1,5 +1,22 @@
-import java.io.*;
-
 public class Main {
     public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
         String archivo = "datos.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                try {
+                    int resultado = calc.evaluarExpresion(linea);
+                    System.out.println("Expresión: " + linea + " = " + resultado);
+                } catch (RuntimeException e) {
+                    System.out.println("Error en la expresión: " + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
+
+        
