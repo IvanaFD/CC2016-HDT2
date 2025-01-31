1a7e2202099d7575588a9fbf54cab7d03bfe9dd0
IvanaFD <ivanafigueroa0936@gmail.com>
2025-01-30 22:34:06 -0600

    documentaion de las clases pilavector y pilavectorTest

--- PilaVector.java
+++ PilaVector.java
@@ -12,34 +12,73 @@
  * Da una implementacion de Pila<T> usando Vector<T>. 
  */
 import java.util.Vector;
+
+/**
+ * Implementación de una pila genérica utilizando la clase {@link Vector}.
+ * @param <T> el tipo de elementos almacenados en la pila
+ */
 public class PilaVector<T> implements Pila<T> {
 
     private Vector<T> pila;
 
+
+     /**
+     * Constructor que inicializa la pila como un {@link Vector}.
+     */
     public PilaVector(){ this.pila = new Vector<T>(); }
     
+
+    /**
+     * Insertar un elemento en la parte superior de la pila.
+     *
+     * @param item elemento a agregar
+     */
     @Override
     public void push(T item) {
         pila.add(item);
     }
 
+
+    /**
+     * Elimina y devuelve el elemento en la parte superior de la pila.
+     *
+     * @return elemento eliminado de la pila
+     * @throws RuntimeException si la pila está vacía
+     */
     @Override
     public T pop() {
         if (isEmpty()) throw new RuntimeException("Pila vacia");
         return pila.remove(pila.size() - 1);
     }
 
+
+    /**
+     * Devuelve el elemento en la parte superior de la pila .
+     *
+     * @return elemento de la parte superior de la pila
+     * @throws RuntimeException si la pila está vacía
+     */
     @Override
     public T peek(){
         if (isEmpty()) throw new RuntimeException("Pila vacia");
         return pila.lastElement();
     }
 
+
+    /**
+     * Verificar si la pila está vacía.
+     * @return true si la pila no tiene elementos, false en caso contrario
+     */
     @Override
     public boolean isEmpty() {
         return pila.isEmpty();
     }
 
+
+    /**
+     * Devuelve el número de elementos que hay en pila.
+     * @return tamaño de la pila
+     */
     @Override
     public int size() {
         return pila.size();
--- PilaVectorTest.java
+++ PilaVectorTest.java
@@ -11,6 +11,10 @@
  * Clase: PilaVectorTest.java
  * Clase de prueba para la clase PilaVector con JUnit
  * 
+ * Métodos probados:
+ * - {@link PilaVector#push(Object)}
+ * - {@link PilaVector#pop()}
+ * - {@link PilaVector#peek()}
  * 
  */
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,19 +22,40 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PilaVectorTest {
+
+    /**
+     * Instancia de la pila para ser utilizada en las pruebas.
+     */
     private PilaVector<Integer> pila;
 
+
+    /**
+     * Configuración inicial antes de cada prueba.
+     * Se crea una nueva instancia de {@link PilaVector}.
+     */
     @BeforeEach
     void setUp() {
         pila = new PilaVector<>();
     }
 
+
+    /**
+     * Prueba el método {@link PilaVector#push(Object)} y {@link PilaVector#pop()}.
+     * Se agrega un elemento a la pila y luego se elimina, verificando que el 
+     * valor devuelto sea el mismo que el agregado.
+     */
     @Test
     void testPusPop(){
         pila.push(10);
         assertEquals(10, pila.pop());
     }
 
+
+    /**
+     * Prueba el método {@link PilaVector#peek()}.
+     * Se agrega el elemento a la pila y luego se verifica que el valor 
+     * en la parte superior de la pila sea el mismo sin eliminarlo de la pila.
+     */
     @Test 
     void testPeek(){
         pila.push(5);
